f18c4087c5981f037d1e1089e1567f9bdbebe443
Romain <romain.clemencon@mycit.ie>
2023-02-12 13:43:59 +0000

    Fixed env settigns

--- src/repositories/dynamodb.py
+++ src/repositories/dynamodb.py
@@ -1,10 +1,13 @@
 import boto3
 import os
 
-access_key = os.environ.get('ACCESS-KEY-TEST')
-secret_key = os.environ.get('SECRET-KEY-TEST')
+access_key = os.environ.get('ACCESS_KEY_TEST')
+secret_key = os.environ.get('SECRET_KEY_TEST')
 
-dynamo_client  =  boto3.resource(service_name = 'dynamodb',region_name = 'eu-west-1',
-              aws_access_key_id = access_key,
-              aws_secret_access_key = secret_key)
+dynamo_client = boto3.resource(
+    service_name = 'dynamodb',
+    region_name = 'eu-west-1',
+    aws_access_key_id = access_key,
+    aws_secret_access_key = secret_key)
+
 
